d5ba9e0b6744934370a8427d4441c9ed62209655
Tadge OBrien <tadgeobrien@gmail.com>
2022-06-27 19:53:07 -0400

    Just the beginnings of this. Need to get the hardward set up too.

---
+++ hiveMonitor_jun27a/hiveMonitor_jun27a.ino
@@ -0,0 +1,33 @@
+#include <HX711.h>
+#include <OneWire.h>
+
+
+
+//HX711 Wiring
+
+// One Wire DS18bs20 wiring
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // Need a TARE function
+  
+  //Check for weight at hour intervals 
+
+
+  //Test line for weight every second.
+
+
+  //Check for Temp at hour intervals 
+
+  //Test line for Temp every second.
+
+
+
+
+}
