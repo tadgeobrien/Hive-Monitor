41fd6f4ade65e297911319c6a34e653e149ca361
Tadge OBrien <tadgeobrien@gmail.com>
2022-06-27 20:41:05 -0400

    Basic wifi ap stuff.

--- sketch_jun27b/sketch_jun27b.ino
+++ sketch_jun27b/sketch_jun27b.ino
@@ -1,4 +1,9 @@
+//https://docs.arduino.cc/tutorials/uno-wifi-rev2/uno-wifi-r2-web-server-ap-mode
+
+
 #include <WiFiNINA.h>
+
+
 
 char ssid[] = "yourNetwork";        // your network SSID (name)
 char pass[] = "yourPassword";    // your network password (use for WPA, or use as key for WEP)
