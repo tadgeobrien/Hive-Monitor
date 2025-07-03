37578c2aa0f9206c32a8f65674c5dd88dcbbd000
Tadge OBrien <tadgeobrien@gmail.com>
2025-07-03 16:46:33 -0400

    Just adding the example clock test data.

---
+++ ClockTest/ClockTest.ino
@@ -0,0 +1,45 @@
+/*
+  Prints the time from the RV-8803 Real Time Clock
+  By: Andy England
+  SparkFun Electronics
+  Date: 2/27/2020
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+  Feel like supporting our work? Buy a board from SparkFun!
+  https://www.sparkfun.com/products/14642
+
+  This example shows how to print the time from the RTC.
+
+  Hardware Connections:
+    Plug the RTC into the Qwiic port on your microcontroller or on your Qwiic shield/adapter.
+    If you are using an adapter cable, here is the wire color scheme:
+    Black=GND, Red=3.3V, Blue=SDA, Yellow=SCL
+    Open the serial monitor at 115200 baud
+*/
+
+#include <SparkFun_RV8803.h> //Get the library here:http://librarymanager/All#SparkFun_RV-8803
+
+RV8803 rtc;
+
+void setup()
+{
+  Wire.begin();
+
+  Serial.begin(9600);
+  
+  
+}
+
+void loop()
+{
+
+    String currentDate = rtc.stringDateUSA(); //Get the current date in mm/dd/yyyy format (we're weird)
+    //String currentDate = rtc.stringDate(); //Get the current date in dd/mm/yyyy format
+    String currentTime = rtc.stringTime(); //Get the time
+    Serial.print(currentDate);
+    //Serial.print(" ");
+    Serial.println(currentTime);
+
+
+  delay(1000);
+}
