589a890683ae0ff31b01c0b2a4d7589bd29217a7
Tadge OBrien <tadgeobrien@gmail.com>
2026-05-25 21:40:30 -0400

    Just added the temp and humidity for inside the hive. Now have to verify that it is working.

--- HiveMonitor-20260525-01/HiveMonitor-20260525-01.ino
+++ HiveMonitor-20260525-01/HiveMonitor-20260525-01.ino
@@ -1,8 +1,20 @@
 //TOBRIEN 20260525 
 // I think I am going to redo a lot of this. I am going to start with just the serial printing and getting logs to openlog.
 
-//Hive Inside Temp and Humidity
+// Wifi Library
+#include <WiFiNINA.h>
 
+
+//Real Time Clock
+#include <SparkFun_RV8803.h>
+
+
+//Hive Inside Temp and Humidity
+#include <SparkFun_SHTC3.h>     //Using for inside Humidity and Temp
+SHTC3 mySHTC3;    // Declare an instance of the SHTC3 class
+float InCel=0;  // Inside Celcius temp
+float InFah=0;  // Inside Farenheit May not use.
+float InHum=0;  // Inside Humidity 
 
 
 // Hive Outside Temp.
@@ -23,14 +35,28 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // Reading the serial monitor.
 
-  //Outside Temp
+  //Inside Hive Temp
+  mySHTC3.begin();  // This calls the SHTC3 sensor to start
+
+
+  //Outside Hive Temp
   sensors.begin();  // This is for OneWire using DallasTemp.
+
+
 
   
 }
 
+
+
 void loop() {
   //Wifi Stuff Will Go here someday
+
+
+  // Hive Inside Temp and Humidity
+  SHTC3_Status_TypeDef result = mySHTC3.update(); //Update the sensor
+
+
   
   //Hive Outside Temp.
   sensors.requestTemperatures(); 
@@ -45,24 +71,19 @@ void loop() {
             20240420,03:20,24,31,1,95
       */
 
-
-
   // Sending to OpenLog
   myLog.begin();
-  myLog.println(OutCel);// FOR ADDING WEIGHT WHEN READY
-
-  
+  myLog.println(String(mySHTC3.toPercent())+","+String(mySHTC3.toDegC())+","+(OutCel));// FOR ADDING WEIGHT WHEN READY
 
 
   // Serial Print 
-  Serial.println((String("Outside Temp C.")+OutCel));// FOR ADDING WEIGHT WHEN READY
+  Serial.println("Inside Humidity Percent"+String(mySHTC3.toPercent())+" Inside Degrees Cel  "+String(mySHTC3.toDegC())+","+(String("Outside Temp C.")+OutCel));// FOR ADDING WEIGHT WHEN READY
 
   myLog.syncFile();
-
 
 // This is what I was working on in the past. It seemed to work, but I may need to tweak it.
 //  Serial.println(String(rtc.stringDateUSA())+","+String(rtc.stringTime())+","+String(mySHTC3.toPercent())+","+String(mySHTC3.toDegC())+","+String(OutCel)+","+String(scale.read()));// FOR ADDING WEIGHT WHEN READY
 
-delay(3000);
+delay(3000); // TOBrien 20260525 Eventually I think I have to change this.
 
 }
